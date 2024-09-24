f517f84c88cd67770c37dca7a555f59b0dc60698
mdriyadtalukder <riyadtalukder67@gmail.com>
2024-09-24 19:53:23 +0600

    update

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
 
         const usersCollection = client.db("socialApp").collection("users");
         const postsCollection = client.db("socialApp").collection("posts");
+        const requestsCollection = client.db("socialApp").collection("requests");
 
 
         // Get users
@@ -82,6 +83,51 @@ async function run() {
             res.send(result)
         })
 
+        //update post num
+        app.patch('/postnumber/:id', async (req, res) => {
+            const data = req.body;
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    post: data.post
+
+                }
+            }
+            const result = await usersCollection.updateOne(query, updatedDoc)
+            res.send(result)
+        });
+
+        //update followers num
+        app.patch('/following/:id', async (req, res) => {
+            const data = req.body;
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    following: data.following
+
+                }
+            }
+            const result = await usersCollection.updateOne(query, updatedDoc)
+            res.send(result)
+        });
+
+        //update followers num
+        app.patch('/followers/:id', async (req, res) => {
+            const data = req.body;
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    followers: data.followers
+
+                }
+            }
+            const result = await usersCollection.updateOne(query, updatedDoc)
+            res.send(result)
+        })
+
 
         // Get posts
         app.get('/posts', async (req, res) => {
@@ -103,6 +149,51 @@ async function run() {
         })
 
 
+        // search request by email query
+        app.get('/requests', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await requestsCollection.find(query).toArray();
+            res.send(result);
+        })
+        //posts add
+        app.post('/requests', async (req, res) => {
+            const item = req.body;
+            const result = await requestsCollection.insertOne(item);
+            res.send(result);
+        })
+
+
+        app.delete('/request/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await requestsCollection.deleteOne(query);
+            res.send(result);
+        })
+        app.get('/followingsposts', async (req, res) => {
+            try {
+                const email = req.query.email;
+
+                // Query to find the user by email
+                const userQuery = { email: email };
+                const user = await usersCollection.findOne(userQuery);
+
+                if (!user || !user.following || !Array.isArray(user.following)) {
+                    return res.status(404).send({ message: "User not found or no followers" });
+                }
+
+                // Get the list of follower emails
+                const followerEmails = user.following;
+
+                // Query the postsCollection to find posts by follower emails
+                const postsQuery = { email: { $in: [email, ...followerEmails] } };
+                const posts = await postsCollection.find(postsQuery).toArray();
+                //console.log(posts)
+                res.send(posts);
+            } catch (error) {
+                res.status(500).send({ message: "Error retrieving followers' posts", error });
+            }
+        });
 
 
 
